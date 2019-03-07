18bc803546be5eaae818207b265b0ce321b119ef
todd <todd@marops.com>
2019-03-07 15:04:19 -0500

    Updated dashboard reporting

--- templatetags/issues_tags.py
+++ templatetags/issues_tags.py
@@ -1,4 +1,7 @@
 from django import template
+from issues.models import Issue, Response
+from django.db.models import Q
+from datetime import datetime, timedelta, timezone
 
 register = template.Library()
 
@@ -11,3 +14,18 @@ def show_responses(issue):
 def issues_response_files(issue_response):
     files = issue_response.document_set.all()
     return {'response_files': files}
+
+@register.inclusion_tag('issues/activity.html')
+def activity(days):
+    dt = datetime.now(timezone.utc) - timedelta(days=days)
+    if days == 1:
+        time_period='24 Hours'
+    else:
+        time_period= f'{days} Days'
+
+    issues=Issue.objects.filter(created_date__gte=dt).filter(completed=False)
+    responses = Response.objects.filter(date__gt=dt)
+    #completed=Issue.objects.filter(completed_date__gte=dt)
+    completed=Issue.objects.filter(completed_date__gte=dt)
+
+    return {'time_period':time_period,'issues':issues,'responses': responses, 'completed':completed}
