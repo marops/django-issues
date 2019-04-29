54c5aedc10db8a11366a814f0b5359cf319949aa
todd <todd@marops.com>
2019-04-29 09:24:36 -0400

    updated admin

--- admin.py
+++ admin.py
@@ -1,11 +1,14 @@
 from django.contrib import admin
 from .models import Issue, Response, Category, Document
 
+class IssueAdmin(admin.ModelAdmin):
+    list_display = ('id', 'short_desc', 'created_date', 'submitted_by','assigned_to')
+
 class ResponseAdmin(admin.ModelAdmin):
     list_display = ('author', 'date', 'snippet')
 
 
-admin.site.register(Issue)
+admin.site.register(Issue, IssueAdmin)
 admin.site.register(Response, ResponseAdmin)
 admin.site.register(Category)
 admin.site.register(Document)
