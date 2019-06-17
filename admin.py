e883505be6e743b275d8f0d37cdbb36b38db9bd0
todd <todd@marops.com>
2019-06-17 17:10:01 -0400

    added issues location table to admin

--- admin.py
+++ admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Issue, Response, Category, Document
+from .models import Issue, Response, Category, Document, Location
 
 class IssueAdmin(admin.ModelAdmin):
     list_display = ('id', 'short_desc', 'created_date', 'submitted_by','assigned_to')
@@ -7,8 +7,12 @@ class IssueAdmin(admin.ModelAdmin):
 class ResponseAdmin(admin.ModelAdmin):
     list_display = ('author', 'date', 'snippet')
 
+class LocationAdmin(admin.ModelAdmin):
+    list_display = ('id', 'lid', 'name')
+
 
 admin.site.register(Issue, IssueAdmin)
 admin.site.register(Response, ResponseAdmin)
 admin.site.register(Category)
 admin.site.register(Document)
+admin.site.register(Location, LocationAdmin)
