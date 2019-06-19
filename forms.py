080e5d02dea9f500c16b3909b4eab507a7ec413d
todd <todd@marops.com>
2019-06-19 13:47:13 -0400

    Updated Issue submitted_by and assigned_to dropdowns to display full name and ordered by name

--- forms.py
+++ forms.py
@@ -1,14 +1,23 @@
 from django.forms import ModelForm
 from .models import Document, Response
 from django import forms
+from django.conf import settings
+from django.db import models
 
 ### ModelForms
 from .models import Issue
+from django.contrib.auth.models import User
 
+class UserModelChoiceField(forms.ModelChoiceField):
+    def label_from_instance(self, obj):
+         return f"{obj.last_name}, {obj.first_name} ({obj.username})"
 
 class IssueForm(forms.ModelForm):
     #due_date=forms.DateField(required=False,widget=forms.TextInput(attrs={'type':'date'}))
     #completed_date=forms.DateTimeField(required=False, widget=forms.TextInput(attrs={'type':'date'}))
+    submitted_by = UserModelChoiceField(User.objects.all().order_by('last_name'))
+    assigned_to = UserModelChoiceField(User.objects.all().order_by('last_name'))
+
     class Meta:
         model=Issue
         #fields=['headline','pub_date','content','reporter']
@@ -18,6 +27,8 @@ class IssueForm(forms.ModelForm):
 class IssueNewForm(forms.ModelForm):
     #due_date=forms.DateField(required=False,widget=forms.TextInput(attrs={'type':'date'}))
     #completed_date=forms.DateTimeField(required=False, widget=forms.TextInput(attrs={'type':'date'}))
+    submitted_by = UserModelChoiceField(User.objects.all().order_by('last_name'))
+
     class Meta:
         model=Issue
         fields=['short_desc','category','desc','location','submitted_by']
